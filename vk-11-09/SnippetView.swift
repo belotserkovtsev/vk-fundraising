4812343171290c95c99640656e67c84526cb02bf
Bogdan <bbelotserkovtsev@gmail.com>
2020-09-12 13:00:32 +0300

    snippet fix

--- vk-11-09/SnippetView.swift
+++ vk-11-09/SnippetView.swift
@@ -26,14 +26,19 @@ struct SnippetContentView: View {
         ZStack{
             RoundedRectangle(cornerRadius: 10)
             .stroke()
-                .opacity(0.18)
+            .opacity(0.08)
+            
+            
+            
+            
             VStack(spacing: .zero){
                 Image("cat")
                     .resizable()
                     .aspectRatio(contentMode: .fill)
-                    .cornerRadius(10)
+                    .frame(width: 359)
+                    .cornerRadius(10, corners: [.topLeft, .topRight])
                     .padding(.bottom, 8)
-                
+//                Spacer()
                 HStack(spacing: .zero) {
                     Text("Добряши помогают котикам")
                         .padding(.leading, 12)
@@ -49,54 +54,65 @@ struct SnippetContentView: View {
                     Spacer()
                 }
                 Divider()
-                    .frame(width: 270)
-                    .padding(.bottom, 8)
+                    .frame(width: 335)
+                    .padding(.bottom, 11.5)
                 
-                HStack(spacing: .zero) {
-                    VStack(spacing: .zero) {
-                        HStack(spacing: .zero) {
-                            Text("Собрано 8 750 ₽ из 10 000 ₽").font(.system(size: 13, weight: .regular))
-                                .padding(.leading, 12)
-                                .padding(.bottom, 8)
-                            Spacer()
-                        }
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 10)
-                            .opacity(0.3)
-                            .foregroundColor(Color("LightButton"))
-                            .frame(width: 237, height: 4)
-                            .padding(.leading, 12)
-                            
-                            HStack(spacing: .zero) {
-                                RoundedRectangle(cornerRadius: 10)
-                                .foregroundColor(Color("LightButton"))
-                                .frame(width: 190, height: 4)
-                                .padding(.leading, 12)
-                                    
-                                
-                                Spacer()
-                            }
-                        }.padding(.bottom, 14)
-                    }
-                    Spacer()
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 10)
-                        .stroke()
-                        Text("Помочь")
-                            .font(.system(size: 14, weight: .medium))
-                    }
-                    .foregroundColor(Color("LightButton"))
-                    .frame(width: 86, height: 30)
-                    .padding(.trailing, 12)
-                    .padding(.bottom, 12)
-                }
+                DonationInfoView()
 //                Spacer()
             }
+
+            
+            
+            
         }.frame(width: 359, height: 140+106, alignment: .leading)
             
     }
     
 }
+
+struct DonationInfoView: View {
+    var body: some View {
+        HStack(spacing: .zero) {
+            VStack(spacing: .zero) {
+                HStack(spacing: .zero) {
+                    Text("Собрано 8 750 ₽ из 10 000 ₽").font(.system(size: 13, weight: .regular))
+                        .padding(.leading, 12)
+                        .padding(.bottom, 8)
+                    Spacer()
+                }
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10)
+                    .opacity(0.3)
+                    .foregroundColor(Color("LightButton2"))
+                    .frame(width: 237, height: 4)
+                    .padding(.leading, 12)
+                    
+                    HStack(spacing: .zero) {
+                        RoundedRectangle(cornerRadius: 10)
+                        .foregroundColor(Color("LightButton2"))
+                        .frame(width: 190, height: 4)
+                        .padding(.leading, 12)
+                            
+                        
+                        Spacer()
+                    }
+                }.padding(.bottom, 14)
+            }
+            Spacer()
+            ZStack {
+                RoundedRectangle(cornerRadius: 10)
+                .stroke()
+                Text("Помочь")  
+                    .font(.system(size: 14, weight: .medium))
+            }
+            .foregroundColor(Color("LightButton2"))
+            .frame(width: 86, height: 30)
+            .padding(.trailing, 12)
+            .padding(.bottom, 12)
+        }
+    }
+}
+
 
 struct SnippetView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ vk-11-09/View+CornerRadius.swift
@@ -0,0 +1,26 @@
+//
+//  View+CornerRadius.swift
+//  vk-11-09
+//
+//  Created by milkyway on 12.09.2020.
+//  Copyright © 2020 belotserkovtsev. All rights reserved.
+//
+
+import SwiftUI
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
+    }
+}
+
+struct RoundedCorner: Shape {
+
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
